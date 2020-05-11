018eb5a1b8faa3a54d00de61bc24a20528ff283a
Christiaan Venter <59892042+codecventer@users.noreply.github.com>
2020-05-11 21:53:18 +0200

    Add files via upload

---
+++ build/precache-manifest.6733ad2bbd4b3e5bb64fd0b48cb38a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfda0c7cf991b14e5aa513a1786a802d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b51a85971e74c43a064",
+    "url": "/static/css/main.ff3a3a3d.chunk.css"
+  },
+  {
+    "revision": "aeafc18c656041078507",
+    "url": "/static/js/2.52b0b57d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52b0b57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b51a85971e74c43a064",
+    "url": "/static/js/main.576d201a.chunk.js"
+  },
+  {
+    "revision": "40406fb2ab9abb87461c",
+    "url": "/static/js/runtime-main.5d2458a9.js"
+  }
+]);
